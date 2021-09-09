5087b876636bb194f789da279e9eda1bddf0103e
Leon Handreke <leonh@ndreke.de>
2021-09-09 15:24:40 +0200

    Add custom pricetag module

---
+++ scb_pricetag/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "scb_pricetag",
+    'summary': """Pricetag Template for SuperCoop Berlin""",
+
+    'description': """""",
+
+    'author': "SuperCoop Berlin",
+    'website': "https://supercoop.de",
+    'version': '12.0',
+    'data': [
+        'views/pricetag.xml',
+        ],
+}
