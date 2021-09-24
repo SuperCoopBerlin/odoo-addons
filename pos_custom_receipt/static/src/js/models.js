12d3c63f657edd4b904eb0785c7048b393eedec5
Thilo Krachenfels <11920747+lichttag@users.noreply.github.com>
2021-09-24 20:19:15 +0200

    [pos_custom_receipt] Compress receipt

--- pos_custom_receipt/static/src/js/models.js
+++ pos_custom_receipt/static/src/js/models.js
@@ -32,4 +32,39 @@ odoo.define('pos_custom_receipt.models', function (require) {
         },
     });
 
+
+    models.Orderline.extend({
+        //Change wrapping width
+        generate_wrapped_product_name: function() {
+            var MAX_LENGTH = 33; // 44 * line ratio of .75
+            var wrapped = [];
+            var name = this.get_product().display_name;
+            var current_line = "";
+    
+            while (name.length > 0) {
+                var space_index = name.indexOf(" ");
+    
+                if (space_index === -1) {
+                    space_index = name.length;
+                }
+    
+                if (current_line.length + space_index > MAX_LENGTH) {
+                    if (current_line.length) {
+                        wrapped.push(current_line);
+                    }
+                    current_line = "";
+                }
+    
+                current_line += name.slice(0, space_index + 1);
+                name = name.slice(space_index + 1);
+            }
+    
+            if (current_line.length) {
+                wrapped.push(current_line);
+            }
+    
+            return wrapped;
+        },
+    });
+
 });
