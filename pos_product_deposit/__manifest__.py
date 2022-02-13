c3deeb183df239dc8edf9b4f2b30261cb1846019
Leon Handreke <leonh@ndreke.de>
2022-02-13 14:48:54 +0100

    Add (bottle) deposit addon for PoS

---
+++ pos_product_deposit/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "pow-product_deposit",
+    'summary': """Attach a deposit to a product that gets automatically added at the PoS""",
+
+    'description': """""",
+
+    'author': "SuperCoop Berlin",
+    'website': "https://supercoop.de",
+    'version': '12.0',
+    'depends': ['product', 'point_of_sale'],
+    'data': [
+        'views/views.xml',
+        'views/assets.xml',
+        ],
+}
